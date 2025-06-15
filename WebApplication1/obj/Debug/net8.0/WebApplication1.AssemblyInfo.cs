3dfbc6200426a351142dd2776f154bd591434d0b
Kardesh36 <121037280+Kardesh36@users.noreply.github.com>
2025-06-15 19:56:01 +0200

    added background image for the pages

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b4e11d1ec13e1e2d976f1ae64a86f64513ed9b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462e5a47cd57f23f65a15cb388b03676d2b99e0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
