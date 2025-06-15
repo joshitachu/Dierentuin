46f0ad927fcdb73520be592722a1218d30fdea42
Kardesh36 <121037280+Kardesh36@users.noreply.github.com>
2025-06-15 17:25:57 +0200

    added css to the home page for a more beautiful look

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42541700b2c525daa3de61aa74c89a80a1d028da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0834f6deed11e8315c073337c774474fa29829f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
