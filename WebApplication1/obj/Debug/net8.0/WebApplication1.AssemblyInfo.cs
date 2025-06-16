0e0d7ef47fbc4e49d90d61223799ca7016dbc3d6
Kardesh36 <121037280+Kardesh36@users.noreply.github.com>
2025-06-16 15:52:56 +0200

    added css to diffrent pages

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462e5a47cd57f23f65a15cb388b03676d2b99e0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfbc6200426a351142dd2776f154bd591434d0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
