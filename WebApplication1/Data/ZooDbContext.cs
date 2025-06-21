8c7334b5f7f45e38bab7ad09a362a454a656f2b6
Kardesh36 <121037280+Kardesh36@users.noreply.github.com>
2025-06-21 20:42:35 +0200

    added seeding data to fill up starter database

--- WebApplication1/Data/ZooDbContext.cs
+++ WebApplication1/Data/ZooDbContext.cs
@@ -1,44 +1,104 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
-
 namespace WebApplication1.Data
 {
-    public class ZooDbContext : DbContext
-    {
-
-        public ZooDbContext(DbContextOptions<ZooDbContext> options)
-                : base(options)
-        {
-
-        }
-
-        private string connectionString = "Server=(localdb)\\mssqllocaldb;Database=Dierentuin1;Trusted_Connection=True;MultipleActiveResultSets=true";
-
-        public DbSet<Animal> Animals { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Enclosure> Enclosures { get; set; }
-        public DbSet<Zoo> Zoos { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(connectionString);
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+public class ZooDbContext : DbContext
 {
-    base.OnModelCreating(modelBuilder);
+public ZooDbContext(DbContextOptions<ZooDbContext> options)
+: base(options)
+{
+}
 
-    modelBuilder.Entity<Category>().HasData(
-        new Category { Id = 1, Name = "Zoogdieren" },
-        new Category { Id = 2, Name = "Vogels" }
-    );
 
-    modelBuilder.Entity<Animal>().HasData(
-    new Animal { Id = 1, Name = "Leeuw", Prey = "Gazelle", Species = "Panthera leo", CategoryId = 1 }
-    
-);
-} 
-        
+    private string connectionString = "Server=(localdb)\\mssqllocaldb;Database=Dierentuin1;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+    public DbSet<Animal> Animals { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Enclosure> Enclosures { get; set; }
+    public DbSet<Zoo> Zoos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Zoogdieren" },
+            new Category { Id = 2, Name = "Vogels" },
+            new Category { Id = 3, Name = "Reptielen" },
+            new Category { Id = 4, Name = "Amfibieën" },
+            new Category { Id = 5, Name = "Vissen" },
+            new Category { Id = 6, Name = "Insecten" },
+            new Category { Id = 7, Name = "Schaaldieren" },
+            new Category { Id = 8, Name = "Weekdieren" },
+            new Category { Id = 9, Name = "Kringloopdieren" },
+            new Category { Id = 10, Name = "Buideldieren" },
+            new Category { Id = 11, Name = "Pinnipedia" },
+            new Category { Id = 12, Name = "Carnivoren" },
+            new Category { Id = 13, Name = "Herbivoren" },
+            new Category { Id = 14, Name = "Omnivoren" },
+            new Category { Id = 15, Name = "Primaten" },
+            new Category { Id = 16, Name = "Carnivora" },
+            new Category { Id = 17, Name = "Cetacea" },
+            new Category { Id = 18, Name = "Chiroptera" },
+            new Category { Id = 19, Name = "Rodentia" },
+            new Category { Id = 20, Name = "Artiodactyla" }
+        );
+
+        modelBuilder.Entity<Zoo>().HasData(
+            new Zoo { Id = 1, Name = "Dierentuin Amsterdam", Location = "Amsterdam" },
+            new Zoo { Id = 2, Name = "Ouwehands Dierenpark", Location = "Rhenen" },
+            new Zoo { Id = 3, Name = "Artis Royal Zoo", Location = "Amsterdam" },
+            new Zoo { Id = 4, Name = "Burgers' Zoo", Location = "Arnhem" },
+            new Zoo { Id = 5, Name = "Safaripark Beekse Bergen", Location = "Hilvarenbeek" },
+            new Zoo { Id = 6, Name = "Blijdorp", Location = "Rotterdam" },
+            new Zoo { Id = 7, Name = "Zoo Rotterdam", Location = "Rotterdam" },
+            new Zoo { Id = 8, Name = "Wilhelma", Location = "Stuttgart" },
+            new Zoo { Id = 9, Name = "Tiergarten Schönbrunn", Location = "Wenen" },
+            new Zoo { Id = 10, Name = "London Zoo", Location = "Londen" },
+            new Zoo { Id = 11, Name = "Berlin Zoologischer Garten", Location = "Berlijn" },
+            new Zoo { Id = 12, Name = "Prague Zoo", Location = "Praag" },
+            new Zoo { Id = 13, Name = "ZooParc de Beauval", Location = "France" },
+            new Zoo { Id = 14, Name = "San Diego Zoo", Location = "San Diego" },
+            new Zoo { Id = 15, Name = "Bronx Zoo", Location = "New York" },
+            new Zoo { Id = 16, Name = "Taronga Zoo", Location = "Sydney" },
+            new Zoo { Id = 17, Name = "Singapore Zoo", Location = "Singapore" },
+            new Zoo { Id = 18, Name = "Copenhagen Zoo", Location = "Kopenhagen" },
+            new Zoo { Id = 19, Name = "Beijing Zoo", Location = "Beijing" },
+            new Zoo { Id = 20, Name = "Edinburgh Zoo", Location = "Edinburgh" }
+        );
+
+        
+
+        modelBuilder.Entity<Animal>().HasData(
+            new Animal { Id = 1, Name = "Leeuw", Prey = "Gazelle", Species = "Panthera leo", CategoryId = 12 },
+            new Animal { Id = 2, Name = "Olifant", Prey = "Planten", Species = "Loxodonta africana", CategoryId = 13 },
+            new Animal { Id = 3, Name = "Papegaai", Prey = "Zaden", Species = "Ara ararauna", CategoryId = 2 },
+            new Animal { Id = 4, Name = "Boa", Prey = "Knaagdieren", Species = "Boa constrictor", CategoryId = 3 },
+            new Animal { Id = 5, Name = "Kikker", Prey = "Insecten", Species = "Rana temporaria", CategoryId = 4 },
+            new Animal { Id = 6, Name = "Goudvis", Prey = "Algen", Species = "Carassius auratus", CategoryId = 5 },
+            new Animal { Id = 7, Name = "Vlinder", Prey = "Nectar", Species = "Danaus plexippus", CategoryId = 6 },
+            new Animal { Id = 8, Name = "Kreeft", Prey = "Detritus", Species = "Homarus gammarus", CategoryId = 7 },
+            new Animal { Id = 9, Name = "Inktvis", Prey = "Visjes", Species = "Octopus vulgaris", CategoryId = 8 },
+            new Animal { Id = 10, Name = "Oehoe", Prey = "Muizen", Species = "Bubo bubo", CategoryId = 2 },
+            new Animal { Id = 11, Name = "Pinguïn", Prey = "Vis", Species = "Aptenodytes forsteri", CategoryId = 2 },
+            new Animal { Id = 12, Name = "Giraffe", Prey = "Bladeren", Species = "Giraffa camelopardalis", CategoryId = 13 },
+            new Animal { Id = 13, Name = "Chimpansee", Prey = "Fruit", Species = "Pan troglodytes", CategoryId = 15 },
+            new Animal { Id = 14, Name = "Walvis", Prey = "Krill", Species = "Balaenoptera musculus", CategoryId = 17 },
+            new Animal { Id = 15, Name = "Vleermuis", Prey = "Insecten", Species = "Myotis myotis", CategoryId = 18 },
+            new Animal { Id = 16, Name = "Muis", Prey = "Zaden", Species = "Mus musculus", CategoryId = 19 },
+            new Animal { Id = 17, Name = "Hert", Prey = "Planten", Species = "Cervus elaphus", CategoryId = 20 },
+            new Animal { Id = 18, Name = "Koe", Prey = "Gras", Species = "Bos taurus", CategoryId = 20 },
+            new Animal { Id = 19, Name = "Wolf", Prey = "Herten", Species = "Canis lupus", CategoryId = 12 },
+            new Animal { Id = 20, Name = "Eend", Prey = "Kleine vis", Species = "Anas platyrhynchos", CategoryId = 2 }
+        );
+    }
+}
+
+
 }
---
+++ WebApplication1/Migrations/20250621184025_full.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class full : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Zoos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zoos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enclosures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Size = table.Column<double>(type: "float", nullable: false),
+                    securityLevel = table.Column<int>(type: "int", nullable: false),
+                    Habitat = table.Column<int>(type: "int", nullable: false),
+                    Climate = table.Column<int>(type: "int", nullable: false),
+                    ZooId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enclosures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Enclosures_Zoos_ZooId",
+                        column: x => x.ZooId,
+                        principalTable: "Zoos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Species = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: true),
+                    AnimalSize = table.Column<int>(type: "int", nullable: false),
+                    Diet = table.Column<int>(type: "int", nullable: false),
+                    activityPattern = table.Column<int>(type: "int", nullable: false),
+                    Prey = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnclosureId = table.Column<int>(type: "int", nullable: true),
+                    SpaceRequirement = table.Column<double>(type: "float", nullable: false),
+                    SecurityRequirement = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Animals_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Animals_Enclosures_EnclosureId",
+                        column: x => x.EnclosureId,
+                        principalTable: "Enclosures",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Zoogdieren" },
+                    { 2, "Vogels" },
+                    { 3, "Reptielen" },
+                    { 4, "Amfibieën" },
+                    { 5, "Vissen" },
+                    { 6, "Insecten" },
+                    { 7, "Schaaldieren" },
+                    { 8, "Weekdieren" },
+                    { 9, "Kringloopdieren" },
+                    { 10, "Buideldieren" },
+                    { 11, "Pinnipedia" },
+                    { 12, "Carnivoren" },
+                    { 13, "Herbivoren" },
+                    { 14, "Omnivoren" },
+                    { 15, "Primaten" },
+                    { 16, "Carnivora" },
+                    { 17, "Cetacea" },
+                    { 18, "Chiroptera" },
+                    { 19, "Rodentia" },
+                    { 20, "Artiodactyla" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Zoos",
+                columns: new[] { "Id", "Location", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Amsterdam", "Dierentuin Amsterdam" },
+                    { 2, "Rhenen", "Ouwehands Dierenpark" },
+                    { 3, "Amsterdam", "Artis Royal Zoo" },
+                    { 4, "Arnhem", "Burgers' Zoo" },
+                    { 5, "Hilvarenbeek", "Safaripark Beekse Bergen" },
+                    { 6, "Rotterdam", "Blijdorp" },
+                    { 7, "Rotterdam", "Zoo Rotterdam" },
+                    { 8, "Stuttgart", "Wilhelma" },
+                    { 9, "Wenen", "Tiergarten Schönbrunn" },
+                    { 10, "Londen", "London Zoo" },
+                    { 11, "Berlijn", "Berlin Zoologischer Garten" },
+                    { 12, "Praag", "Prague Zoo" },
+                    { 13, "France", "ZooParc de Beauval" },
+                    { 14, "San Diego", "San Diego Zoo" },
+                    { 15, "New York", "Bronx Zoo" },
+                    { 16, "Sydney", "Taronga Zoo" },
+                    { 17, "Singapore", "Singapore Zoo" },
+                    { 18, "Kopenhagen", "Copenhagen Zoo" },
+                    { 19, "Beijing", "Beijing Zoo" },
+                    { 20, "Edinburgh", "Edinburgh Zoo" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "AnimalSize", "CategoryId", "Diet", "EnclosureId", "Name", "Prey", "SecurityRequirement", "SpaceRequirement", "Species", "Status", "activityPattern" },
+                values: new object[,]
+                {
+                    { 1, 0, 12, 0, null, "Leeuw", "Gazelle", 0, 0.0, "Panthera leo", null, 0 },
+                    { 2, 0, 13, 0, null, "Olifant", "Planten", 0, 0.0, "Loxodonta africana", null, 0 },
+                    { 3, 0, 2, 0, null, "Papegaai", "Zaden", 0, 0.0, "Ara ararauna", null, 0 },
+                    { 4, 0, 3, 0, null, "Boa", "Knaagdieren", 0, 0.0, "Boa constrictor", null, 0 },
+                    { 5, 0, 4, 0, null, "Kikker", "Insecten", 0, 0.0, "Rana temporaria", null, 0 },
+                    { 6, 0, 5, 0, null, "Goudvis", "Algen", 0, 0.0, "Carassius auratus", null, 0 },
+                    { 7, 0, 6, 0, null, "Vlinder", "Nectar", 0, 0.0, "Danaus plexippus", null, 0 },
+                    { 8, 0, 7, 0, null, "Kreeft", "Detritus", 0, 0.0, "Homarus gammarus", null, 0 },
+                    { 9, 0, 8, 0, null, "Inktvis", "Visjes", 0, 0.0, "Octopus vulgaris", null, 0 },
+                    { 10, 0, 2, 0, null, "Oehoe", "Muizen", 0, 0.0, "Bubo bubo", null, 0 },
+                    { 11, 0, 2, 0, null, "Pinguïn", "Vis", 0, 0.0, "Aptenodytes forsteri", null, 0 },
+                    { 12, 0, 13, 0, null, "Giraffe", "Bladeren", 0, 0.0, "Giraffa camelopardalis", null, 0 },
+                    { 13, 0, 15, 0, null, "Chimpansee", "Fruit", 0, 0.0, "Pan troglodytes", null, 0 },
+                    { 14, 0, 17, 0, null, "Walvis", "Krill", 0, 0.0, "Balaenoptera musculus", null, 0 },
+                    { 15, 0, 18, 0, null, "Vleermuis", "Insecten", 0, 0.0, "Myotis myotis", null, 0 },
+                    { 16, 0, 19, 0, null, "Muis", "Zaden", 0, 0.0, "Mus musculus", null, 0 },
+                    { 17, 0, 20, 0, null, "Hert", "Planten", 0, 0.0, "Cervus elaphus", null, 0 },
+                    { 18, 0, 20, 0, null, "Koe", "Gras", 0, 0.0, "Bos taurus", null, 0 },
+                    { 19, 0, 12, 0, null, "Wolf", "Herten", 0, 0.0, "Canis lupus", null, 0 },
+                    { 20, 0, 2, 0, null, "Eend", "Kleine vis", 0, 0.0, "Anas platyrhynchos", null, 0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_CategoryId",
+                table: "Animals",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animals_EnclosureId",
+                table: "Animals",
+                column: "EnclosureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enclosures_ZooId",
+                table: "Enclosures",
+                column: "ZooId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Enclosures");
+
+            migrationBuilder.DropTable(
+                name: "Zoos");
+        }
+    }
+}
